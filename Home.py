0ce78a488ffdf32d697cd5f6666a8ba119ee239c
Guillermo Cerato <136848600+Willy71@users.noreply.github.com>
2024-09-07 10:19:44 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -105,7 +105,9 @@ def main():
         frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
     </div>
     """, unsafe_allow_html=True)
-
+    
+    st.text("")
+    
     # Botón para eliminar el video
     with st.container():
         col15, col16, col17, col18, col19 = st.columns([3,1,1,1,2])
